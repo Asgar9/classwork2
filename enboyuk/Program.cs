5ddad1b004c8602d2b29602ac3f0c127e705375f
Asgar Y. Asgarov <asgaryas@code.local>
2022-08-05 20:06:36 +0400

    bax niye boyuk berberdir olmalidir

---
+++ enboyuk/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace enboyuk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("ededleri daxil edin:");
+            Console.WriteLine(bigger(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+        }
+        static int bigger(int num1,int num2,int num3)
+        {
+            if (num1>num2 && num1>num3 )
+            {
+                return num1;
+            }
+            if(num2>num1 && num2 > num3)
+            {
+                return num2;
+            }
+            else
+            {
+                return num3;
+            }
+        }
+    }
+}
